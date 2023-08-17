e5e5bd0c5da78598305421a87c0442e08b76553b
gamma82 <davidlitchfield.work@gmail.com>
2023-08-17 23:16:45 +0100

    Added animation to the figure, but still needs fps buff

--- program.py
+++ program.py
@@ -23,12 +23,60 @@ def factorial(n):
     return total
 
 def F(n,k):
+    
+    bottomleft = 2*k-n
+    bottomright = n-k
+    
+    if bottomleft < 0 or bottomright < 0:
+        return 0
+    
+    minimum = min(bottomleft,bottomright)
+    maximum = max(bottomleft,bottomright)
+    
+    total = 1
+    
+    divide = []
+    mult = []
+    
+    for i in range(1, minimum + 1):
+        
+        divide.append( i )
+        
+
+          
+    for i in range(maximum+1, k+1):
+        mult.append(i)
+        
+      
+    if len(divide) > len(mult):
+        
+        for i in range(len(mult)):
+            
+            total *= (mult[i] / divide[i])
+            
+        for j in range(len(mult), len(divide) ):
+            total /= divide[j]
+            
+    else:
+        
+        for i in range(len(divide)):
+            
+            total *= (mult[i] / divide[i])
+            
+        for j in range(len(divide), len(mult) ):
+            total *= mult[j]
+        
+    
+    return total * (lam ** bottomleft) * (mu ** bottomright)
+    
+
+    
     return  ( factorial(k) * ( lam ** (2*k-n) ) * ( mu ** (n-k) ) ) / ( factorial(2*k-n) * factorial(n-k) ) 
 
-def recurrence(n):
+def recurrence(n,ktimes):
     total =0
     
-    for k in range(1,n-1 +1):
+    for k in range(1,ktimes +1):
         total += ( F(n-2,k-1) * mu * u0 + F(n-1,k) * u1)
         
     return total
@@ -94,41 +142,80 @@ from matplotlib import pyplot as plt
 import numpy as np
 from matplotlib.animation import FuncAnimation
 
-extent = 25
+
+extent = 30
 sign = "-" if mu < 0 else "+"
 
 fig, (ax1,ax2,ax3) = plt.subplots(ncols=3,nrows=1)
 
-recurrence_from_0_to_extent = first_n_numbers_of_recurrence(extent) 
-my_recurrence_version = [recurrence(i) for i in range(0, extent)]
-greg_numbers = [gregory_newton(n, recurrence_from_0_to_extent, extent-1) for n in range(0,24+1)]
-
-
 ax1.grid(True)
-ax2.grid(True)
-ax3.grid(True)
-
-
-
 
 x = np.arange(0,extent)
 
-growth_rate_limiter = lambda x : x ** (1/3)
+growth_rate_limiter = lambda x : x ** (1/9)
 
-ax1plot_original = ax1.plot(x, [ growth_rate_limiter(x) for x in recurrence_from_0_to_extent] , color="green")
-my_rec_values = ax2.plot(x, [growth_rate_limiter(x) for x in my_recurrence_version], color="red")
-greg_formula = ax3.plot(x, [ growth_rate_limiter(x) for x in greg_numbers], color = "blue")
 
-ax1.set_ylabel("u(n)", fontsize=15)
-ax1.set_xlabel("n (original recurrence)" , fontsize=15)
 
-ax2.set_xlabel("Finite series approximation")
-ax3.set_xlabel("Gregory-Newton approximation")
+title = plt.suptitle(f"Approximating the recurrence: u(n) = {lam}u(n-1) {sign} {abs(mu)}u(n-2)")
+    
+recurrence_from_0_to_extent = first_n_numbers_of_recurrence(extent) 
+ax1plot_original = ax1.plot(x, [ growth_rate_limiter(x) for x in recurrence_from_0_to_extent] , color="green")    
 
-ax3.yaxis.set_label_position("right")
-ax3.set_ylabel(f"(data coded: f(x) = cbrt(x))")
 
-plt.suptitle(f"Approximating the recurrence: u(n) = {lam}u(n-1) {sign} {abs(mu)}u(n-2)")
+my_recurrence_frames = []
+gregs_frames = []
+
+for t in range(extent):
+    
+    
+    my_recurrence_version = [recurrence(n, t) for n in range(0, extent)]
+    greg_numbers = [gregory_newton(n, recurrence_from_0_to_extent, t) for n in range(0,extent)]
+    
+    my_recurrence_frames.append(my_recurrence_version)
+    gregs_frames.append(greg_numbers)
+    
+    
+
+
+
+def animate(t):
+
+    global my_reccurrence_frames
+    global gregs_frames
+
+    ax2.cla()
+    ax3.cla()
+    
+    ax2.grid(True)
+    ax3.grid(True)
+    
+    title = plt.suptitle(f"Approximating the recurrence: u(n) = {lam}u(n-1) {sign} {abs(mu)}u(n-2), t = {t}")
+    
+    ax1.set_ylabel("u(n)", fontsize=15)
+    ax1.set_xlabel("n (original recurrence)" , fontsize=15)
+
+    ax2.set_xlabel("Finite series approximation")
+    ax3.set_xlabel("Gregory-Newton approximation")
+
+    ax3.yaxis.set_label_position("right")
+    ax3.set_ylabel(f"(data coded: f(x) = cbrt(x))")
+
+
+    
+
+
+
+   
+    my_rec_values = ax2.plot(x, [growth_rate_limiter(x) for x in my_recurrence_frames[t] ], color="red")
+    greg_formula = ax3.plot(x, [ growth_rate_limiter(x) for x in gregs_frames[t]], color = "blue")
+
+
+
+anim = FuncAnimation(fig, animate, extent, interval=10, repeat_delay = 2500)
+
+
+
+
 
 plt.show()
             
