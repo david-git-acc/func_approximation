5223f57f6bf1a29dd2726026bd774d1ae231a4f8
gamma82 <davidlitchfield.work@gmail.com>
2023-08-17 20:44:39 +0100

    Added the code for the Gregory Newton formula (0-indexed only)

--- program.py
+++ program.py
@@ -32,6 +32,45 @@ def recurrence(n):
         
     return total
 
+def binomial(n,k):
+    return factorial(n) / ( factorial(k) * factorial(n-k) ) 
 
-for i in range(0,25):
-    print(i , recurrence(i))
+
+def gregory_newton(n, numbers, kthdifference):
+    
+    if kthdifference > len(numbers):
+        return None
+    
+    difference_lists = [numbers[0:kthdifference+1] ]
+    
+    
+    for ithdifference in range(kthdifference):
+        
+        next_list = []
+        last_list = difference_lists[-1]
+        
+        
+        for i in range(0, len(last_list) - 2 + 1):
+            
+            this_diff = last_list[i+1] - last_list[i]
+            
+            next_list.append(this_diff)
+            
+        difference_lists.append(next_list)    
+            
+    total = 0            
+
+    for i in range(0,len(difference_lists)):
+        
+        total += difference_lists[i][0] * binomial(n, i)
+        
+    return total
+            
+        
+thenumbers = [x**2 - 3*x + 1 for x in range(0,5)]
+
+print(gregory_newton(35, thenumbers, 3))
+            
+            
+            
+    
